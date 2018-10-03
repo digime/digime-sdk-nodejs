f188e82b9547a8dfb278e277b5d8ed0539e629de
Ogi <ognjen.bekic@gmail.com>
2018-10-03 21:07:36 +0200

    SDK 0.1.0 Initial version of SDK
    Co-authored-by: Dražen Tenžera <drazen@digi.me>
    Co-authored-by: Ricky <ricky@digi.me>

--- README.md
+++ README.md
@@ -1,1 +1,160 @@
-# digime-js-sdk
+# Digi.me SDK for javascript
+---
+The Digi.me SDK for Javascript/Typescript is a Node library that allows seamless authentication with Consent Access service, making content requests and core decryption services. For details on the API and general CA architecture - visit [Dev Support Docs](https://developers.digi.me/consent-access.html)
+
+## Prerequisite 
+In order to run the complete Consent Access process, the Digi.me SDK for javascript required the Digi.me app being installed on either an iOS or Android device to enable user authorization of requests. For detailed explanation of the Consent Acess architecture - visit [Dev Support Docs](https://developers.digi.me/consent-access.html)
+
+## Requirements
+- Node 8 or above
+
+## Table of Contents
+
+  * [Installation](#installation)
+  * [SDK usage](#sdk-usage)
+
+## Installation
+Details to come.
+
+## SDK Usage
+### Obtaining your Contract ID/App ID/Private Key
+
+Before accessing the public APIs, a valid Contract ID needs to be registered for an App ID.
+The Contract ID uniquely identifies a contract with the user that spells out what type of data you want, what you will and won't do with it, how long you will retain the data and whether you will implement the right to be forgotten.
+
+Additionally it also specifies how the data is encrypted in transit.
+
+A private key is needed to decrypt data that is passed back. Digi.me can provide you with a private ID.
+
+To register a Consent Access contract check out [Digi.me Dev Support](https://developers.digi.me). There you can request a Contract ID and App ID to which it is bound.
+
+### Configuring the SDK
+When initialising the SDK, you have the ability to override the default behaviour and specify some options. 
+The create SDK function has the following signature:
+```typescript
+    const createSDK = (sdkOptions?: Partial<DigiMeSDKConfiguration>);
+```
+
+##### DigiMeSDKConfiguration 
+Options you can configure when initialising the SDK:
+```typescript
+interface DigiMeSDKConfiguration {
+    host: string;
+    version: string;
+    retryOptions: PartialAttemptOptions<any>;
+    scope: CAScope;
+}
+```
+`host` 
+Type: string 
+The environment of digi.me to point to. By default it will use the production environment of digi.me. Unless specifically instructed, it is best to use this environment as it will be the most stable. Default: "api.digi.me"
+
+`version` Type: string
+The version of the public api to point to. Default: "v1.0"
+
+`retryOptions` Type: PartialAttemptOptions<any>
+Options to specify retry logic for failed API calls
+
+`scope` Type : CAScope
+Options to only return a subset of data the contract asks for. Default: {}
+
+##### [PartialAttemptOptions] (https://github.com/lifeomic/attempt/blob/master/src/index.ts#L14-L27)
+
+##### CAScope
+```typescript
+interface CAScope {
+    timeRanges? : ITimeRange[];
+}
+```
+`timeRanges` 
+Type: ITimeRange[]
+Having timeRanges set will allow you to retrieve only a subset of data that the contract has asked for. This might come in handy if you already have data from the existing user and you might only want to retrieve any new data that might have been added to the user's library in the last x months. The format of ITimeRange is as follows:
+
+##### ITimeRange
+```typescript
+interface ITimeRange {
+    from?: number;
+    last?: string;
+    to?: number;
+}
+```
+
+`from` 
+Type: number
+This is the unix timestamp in seconds. If this is set, we will return data created after this timestamp.
+
+`to` 
+Type: number
+This is the unix timestamp in seconds. If this is set, we will return data created before this timestamp.
+
+`last`
+Type: string
+You can set a dynamic time range based on the current date. The string is in the format of "<value><unit>"
+For units we currently accept:
+
+'d' - day
+'m' - month
+'y' - year
+
+For example to return data for the last six months : "6m"
+
+
+
+### Establishing a session
+To start fetching data into your application, you will need to authorise a session.
+The authorisation flow is separated into two phases:
+
+Initialise a session with Digi.me API which returns a session object.
+```typescript
+    establishSession = async (appId: string, contractId: string, options: DigiMeSDKConfiguration): Promise<Session>;
+```
+
+### Getting User Consent
+In digi.me we provide two different ways to prompt user for consent
+1. Existing users who already have the digi.me application installed - Use the `getAppURL` method to get a URL which can be used to trigger the digi.me client to open on their Android or iOS devices. The callbackURL you pass in will be the URL the digi.me client will call once the user has accepted the data request. Given the session id, the client will know the details of the contract and ask for the user's permission on only the data the contract needs.
+    ```typescript
+    getAppURL = (appId: string, session: Session, callbackURL: string);
+    ```
+
+2. Guest consent - This is a demo feature which allows the user to consent and onboard to digi.me using the browser. To trigger this onboard mode, you can call the `getWebURL` method to get a URL which when opened will ask user for consent.
+    ```typescript
+    getWebURL = (session: Session, callbackURL: string, options: DigiMeSDKConfiguration);
+    ```
+
+Regardless of which mode above you've trigger, the callbackURL will be triggered once the user has authorised the consent. The callbackURL will be triggered with a new param `result` where the value will either be `DATA_READY` or `CANCELLED` if the user decided to deny the request.
+
+### Fetching Data
+Upon successful authorisation you can now request user's files. To fetch all available data for your contract you can call `getDataForSession` to start your data fetch. You'll need to provide us with a private key with which we will try and decrypt user data. In addition you can pass a onFileData and onFileError which will be triggered whenever a user data file is processed or if the fetch errored out.
+```typescript
+getDataForSession = async (
+    sessionKey: string,
+    privateKey: NodeRSA.Key,
+    onFileData: FileSuccessHandler,
+    onFileError: FileErrorHandler,
+    options: DigiMeSDKConfiguration
+): Promise<any>;
+```
+
+The `onFileData` callback function is triggered whenever we have received data from the server. The content is passed back in files, so this function will be triggered whenever we have received data from one file. The callback will be triggered with an object in the parameter. The object has the following properties: 
+    1. fileData - JSON string of data objects
+    2. fileName - the filename which the objects reside in
+    3. fileList - the list of all files that are to be returned
+```typescript
+callback = ({
+    fileData: any, 
+    fileName: string, 
+    fileList: string[],
+}): void;
+```
+
+The `onFileError` callback function is triggered whenever we have failed to receive any data for the data file. By default, we try to fetch the data five times with exponential backoff before invoking the error callback. The error callback will be triggered with an object in the parameter. The object has the following properties: 
+    1. error - error object
+    2. fileName - the filename which the objects reside in
+    3. fileList - the list of all files that are to be returned
+```typescript
+callback = ({
+    error: Error, 
+    fileName: string, 
+    fileList: string[],
+}): void;
+```
---
+++ src/sdk.ts
@@ -0,0 +1,248 @@
+/*!
+ * Copyright (c) 2009-2018 digi.me Limited. All rights reserved.
+ */
+
+import { PartialAttemptOptions, retry } from "@lifeomic/attempt";
+import { decompress } from "iltorb";
+import isFunction from "lodash.isfunction";
+import isInteger from "lodash.isinteger";
+import isPlainObject from "lodash.isplainobject";
+import isString from "lodash.isstring";
+import NodeRSA from "node-rsa";
+import { decryptData } from "./crypto";
+import { net } from "./net";
+
+interface DigiMeSDKConfiguration {
+    host: string;
+    version: string;
+    retryOptions?: PartialAttemptOptions<any>;
+}
+
+interface Session {
+    expiry: number;
+    sessionKey: string;
+}
+
+interface FileMeta {
+    fileData: any;
+    fileName: string;
+}
+
+interface CAScope {
+    timeRanges?: ITimeRange[];
+}
+
+interface ITimeRange {
+    from?: number;
+    last?: string;
+    to?: number;
+}
+
+interface IGetFileResponse {
+    fileContent: string;
+    compression?: string;
+}
+
+type FileSuccessResult = { data: any } & FileMeta;
+type FileErrorResult = { error: Error } & FileMeta;
+type FileSuccessHandler = (response: FileSuccessResult) => void;
+type FileErrorHandler = (response: FileErrorResult) => void;
+
+// TODO: replace with build step
+const SDK_VERSION = "0.1.0";
+
+const _establishSession = async (
+    appId: string,
+    contractId: string,
+    options: DigiMeSDKConfiguration,
+    scope?: CAScope,
+): Promise<Session> => {
+    if (!isString(appId)) {
+        throw new Error("Parameter appId should be string");
+    }
+    if (!isString(contractId)) {
+        throw new Error("Parameter contractId should be string");
+    }
+    const url = `https://${options.host}/${options.version}/permission-access/session`;
+
+    const response = await net.post(url, {
+        json: true,
+        body: {
+            appId,
+            contractId,
+            scope,
+            accept: {
+                compression: "brotli",
+            },
+        },
+    });
+
+    return response.body;
+};
+
+const _getWebURL = (session: Session, callbackURL: string, options: DigiMeSDKConfiguration) => {
+    if (!_isSessionValid(session)) {
+        throw new Error("Session should be an object that contains expiry as number and sessionKey property as string");
+    }
+    if (!isString(callbackURL)) {
+        throw new Error("Parameter callbackURL should be string");
+    }
+    // tslint:disable-next-line:max-line-length
+    return `https://${options.host}/apps/quark/direct-onboarding?sessionKey=${session.sessionKey}&callbackUrl=${encodeURIComponent(callbackURL)}`;
+};
+
+const _getAppURL = (appId: string, session: Session, callbackURL: string) => {
+    if (!_isSessionValid(session)) {
+        throw new Error("Session should be an object that contains expiry as number and sessionKey property as string");
+    }
+    if (!isString(callbackURL)) {
+        throw new Error("Parameter callbackURL should be string");
+    }
+    if (!isString(appId)) {
+        throw new Error("Parameter appId should be string");
+    }
+    // tslint:disable-next-line:max-line-length
+    return `digime://consent-access?sessionKey=${session.sessionKey}&callbackURL=${encodeURIComponent(callbackURL)}&appId=${appId}&sdkVersion=${SDK_VERSION}`;
+};
+
+const _getFileList = async (sessionKey: string, options: DigiMeSDKConfiguration): Promise<string[]> => {
+    const url = `https://${options.host}/${options.version}/permission-access/query/${sessionKey}`;
+    const response = await net.get(url, {json: true});
+
+    return response.body.fileList;
+};
+
+const _getFile = async (
+    sessionKey: string,
+    fileName: string,
+    options: DigiMeSDKConfiguration,
+): Promise<IGetFileResponse> => {
+    const url = `https://${options.host}/${options.version}/permission-access/query/${sessionKey}/${fileName}`;
+    const response = await retry(async () => net.get(url, {json: true}), options.retryOptions);
+    const {fileContent, compression} = response.body;
+
+    return {
+        compression,
+        fileContent,
+    };
+};
+
+const _getDataForSession = async (
+    sessionKey: string,
+    privateKey: NodeRSA.Key,
+    onFileData: FileSuccessHandler,
+    onFileError: FileErrorHandler,
+    options: DigiMeSDKConfiguration,
+): Promise<any> => {
+
+    if (!isString(sessionKey)) {
+        throw new Error("Parameter sessionKey should be string");
+    }
+
+    // Set up key
+    const key: NodeRSA = new NodeRSA(
+        privateKey,
+        "pkcs1-private-pem",
+    );
+
+    const fileList = await _getFileList(sessionKey, options);
+    const filePromises = fileList.map((fileName) => {
+
+        return _getFile(sessionKey, fileName, options).then(async (fileData: IGetFileResponse) => {
+            const {compression, fileContent} = fileData;
+            let data: Buffer = decryptData(key, fileContent);
+
+            if (compression === "brotli") {
+                data = await decompress(data);
+            }
+
+            data = JSON.parse(data.toString("utf8"));
+
+            if (isFunction(onFileData)) {
+                onFileData({
+                    fileData: data,
+                    fileName,
+                    fileList,
+                });
+            }
+            return;
+        }).catch((error) => {
+            // Failed all attempts
+            if (isFunction(onFileError)) {
+                onFileError({
+                    error,
+                    fileName,
+                    fileList,
+                });
+            }
+            return;
+        });
+    });
+
+    await Promise.all(filePromises);
+    return;
+};
+
+const _isSessionValid = (session: unknown): session is Session => (
+    _isPlainObject(session) && isInteger(session.expiry) && isString(session.sessionKey)
+);
+
+const _areOptionsValid = (options: unknown): options is DigiMeSDKConfiguration => (
+    _isPlainObject(options) && isString(options.host) && isString(options.version)
+);
+
+const _isPlainObject = (o: unknown): o is { [key: string]: unknown } => isPlainObject(o);
+
+const createSDK = (sdkOptions?: Partial<DigiMeSDKConfiguration>) => {
+
+    if (sdkOptions !== undefined && !_isPlainObject(sdkOptions)) {
+        throw new Error("SDK options should be object that contains host and version properties");
+    }
+
+    const options: DigiMeSDKConfiguration = {
+        host: "api.digi.me",
+        version: "v1.0",
+        retryOptions: {
+            delay: 750,
+            factor: 2,
+            maxAttempts: 5,
+        },
+        ...sdkOptions,
+    };
+
+    if (!_areOptionsValid(options)) {
+        throw new Error("SDK options should be object that contains host and version properties as string");
+    }
+
+    return {
+        establishSession: (
+            appId: string,
+            contractId: string,
+            scope?: CAScope,
+        ) => (
+            _establishSession(appId, contractId, options, scope)
+        ),
+        getDataForSession: (
+            sessionKey: string,
+            privateKey: NodeRSA.Key,
+            onFileData: FileSuccessHandler,
+            onFileError: FileErrorHandler,
+        ) => (
+            _getDataForSession(sessionKey, privateKey, onFileData, onFileError, options)
+        ),
+        getWebURL: (session: Session, callbackURL: string) => _getWebURL(session, callbackURL, options),
+        getAppURL:  (appId: string, session: Session, callbackURL: string) => _getAppURL(appId, session, callbackURL),
+    };
+};
+
+export {
+    createSDK,
+    CAScope,
+    FileMeta,
+    FileSuccessResult,
+    FileErrorResult,
+    FileSuccessHandler,
+    FileErrorHandler,
+    Session,
+    DigiMeSDKConfiguration,
+};
