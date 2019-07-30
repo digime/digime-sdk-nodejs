2b0a637f07ca7065689195222fc3fb0e7159d164
Ricky <ricky@digi.me>
2019-07-30 15:27:11 +0100

    Add support for implicit sync
    This commit updates the getSessionData function to make use of the new
    implicit sync in the digi.me backend. The function now polls an endpoint
    to see whether there are new files available to fetch as the data is still syncing.
    
    Also updated:
    - The link to a version of quark which supports implicit
    sync.
    - Bumped the version of the digi.me backend to v1.4.
    - Updated the source code header to year 2019

---
+++ src/__mocks__/utils.ts
@@ -0,0 +1,16 @@
+/*!
+ * Copyright (c) 2009-2019 digi.me Limited. All rights reserved.
+ */
+
+// NOTE: This mock is added to overcome jest timer mocks not working!!
+
+const {isValidString, isPlainObject, isSessionValid, isConfigurationValid} = jest.requireActual("../utils");
+
+export {
+    isValidString,
+    isPlainObject,
+    isSessionValid,
+    isConfigurationValid,
+};
+
+export const sleep = () => Promise.resolve();
---
+++ src/api-responses.ts
@@ -0,0 +1,29 @@
+/*!
+ * Copyright (c) 2009-2019 digi.me Limited. All rights reserved.
+ */
+
+export interface GetFileListResponse {
+    status: {
+        state: LibrarySyncStatus;
+        details: unknown[];
+    };
+    fileList: Array<{
+        name: string;
+        updatedDate: number;
+    }>;
+}
+
+export interface GetFileResponse {
+    fileContent: string;
+    fileDescriptor: {
+        objectCount: number;
+        objectType: string;
+        serviceGroup: string;
+        serviceName: string;
+        mimetype?: string;
+    };
+    compression?: string;
+}
+
+export type AccountSyncStatus = "running" | "partial" | "completed";
+export type LibrarySyncStatus = AccountSyncStatus | "pending";
