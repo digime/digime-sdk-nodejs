766bfb0e2de02dc7a2b2894ba9c3e0663aa07821
Ricky <ricky@digi.me>
2019-10-01 13:48:42 +0100

    Bumped version to rc.0

--- src/sdk-version.ts
+++ src/sdk-version.ts
@@ -2,4 +2,8 @@
  * Copyright (c) 2009-2019 digi.me Limited. All rights reserved.
  */
 
-export default "1.0.0";
+/*!
+* Copyright (c) 2009-2018 digi.me Limited. All rights reserved.
+*/
+
+export default "1.0.0-rc.0";
