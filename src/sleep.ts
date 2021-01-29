af664df5402afc8621fa860bb868a0c5361512fa
Ogi <ognjen.bekic@gmail.com>
2021-01-29 13:22:19 +0100

    Update copyright year from 2020 to 2021

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2009-2020 digi.me Limited. All rights reserved.
+ * Copyright (c) 2009-2021 digi.me Limited. All rights reserved.
  */
 
 export const sleep = (milliseconds: number) => new Promise((resolve) => setTimeout(resolve, milliseconds));
