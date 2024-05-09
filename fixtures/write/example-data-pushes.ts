719c609c2f867dbd6ecbe90ba7ca24c5001cd7a5
Ognjen Bekic <ognjen.bekic@gmail.com>
2024-05-09 09:48:50 +0200

    Update copyright year to 2024

--- fixtures/write/example-data-pushes.ts
+++ fixtures/write/example-data-pushes.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2009-2023 World Data Exchange Holdings Pty Limited (WDXH). All rights reserved.
+ * Copyright (c) 2009-2024 World Data Exchange Holdings Pty Limited (WDXH). All rights reserved.
  */
 
 import NodeRSA from "node-rsa";
